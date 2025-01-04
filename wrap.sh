20828aa34aea80c9b43d00bb2f9dff85835d71e1
Aswin Pradeep <100145929+aswinpradeepc@users.noreply.github.com>
2025-01-04 20:30:14 +0530

    Update wrap.sh

--- wrap.sh
+++ wrap.sh
@@ -6,20 +6,14 @@ sum_intervals=0
 
 for ((i=1; i<=total_commits; i++))
 do
-    # Calculate the remaining time to meet the desired sum
     remaining_time=$((desired_sum_intervals - sum_intervals))
-
-    # Ensure the interval does not exceed the remaining time
     interval=$((RANDOM % remaining_time + 1))
     echo "Interval: $interval seconds"
-
     sum_intervals=$((sum_intervals + interval))
-
     echo "Added something to the file - $i" >> $file
     git add $file
     git commit -m "Commit $i: Added something to the file"
     echo "Commit $i: Added something to the file"
-
     sleep $interval
 done
 
